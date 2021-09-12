af1f2591ae62b0aa0d5f07dc7f47445a40526341
Dennis <Dennis@DESKTOP-OFS21UG>
2021-09-12 18:48:25 +0200

    Projektdateien hinzufügen.

---
+++ Client/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'TextBox2
+        '
+        Me.TextBox2.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.TextBox2.BackColor = System.Drawing.Color.DimGray
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.TextBox2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBox2.Font = New System.Drawing.Font("Unispace", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox2.ForeColor = System.Drawing.Color.Black
+        Me.TextBox2.ImeMode = System.Windows.Forms.ImeMode.Disable
+        Me.TextBox2.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.ShortcutsEnabled = False
+        Me.TextBox2.Size = New System.Drawing.Size(482, 373)
+        Me.TextBox2.TabIndex = 10
+        Me.TextBox2.WordWrap = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(482, 373)
+        Me.Controls.Add(Me.TextBox2)
+        Me.ForeColor = System.Drawing.Color.Black
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.TopMost = True
+        Me.TransparencyKey = System.Drawing.Color.DimGray
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+        Me.SetTopLevel(True)
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ Client/Form1.vb
@@ -0,0 +1,140 @@
+﻿Imports MSI.Afterburner
+Imports MSI.Afterburner.Exceptions
+Imports System.Drawing
+
+
+
+Public Class Form1
+    Dim active As Boolean = True
+    Dim hwi As New HardwareMonitor
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If (My.Settings.X = 0) Then
+        Else
+
+            Me.SetDesktopLocation(My.Settings.X, My.Settings.Y)
+        End If
+        Timer1.Enabled = True
+
+    End Sub
+    Private Sub Rohdaten()
+        Try
+
+
+
+
+            Dim Result As String = ""
+
+            Result = hwi.Header.ToString().Replace(";", Environment.NewLine)
+            For Each item As HardwareMonitorEntry In hwi.Entries
+                Result &= Environment.NewLine & "#####################################" & Environment.NewLine
+                Result &= item.ToString().Replace(";", Environment.NewLine)
+            Next
+
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+
+    Private Function SetComplementary() As Bitmap
+        Dim bmp As New Bitmap(Me.Size.Width, Me.Size.Height)
+        Graphics.FromImage(bmp).CopyFromScreen(Me.DesktopLocation, New Point(0, 0), Me.Size)
+        TextBox2.ForeColor = Color.FromArgb(bmp.GetPixel(Size.Width / 2, Size.Height / 2).ToArgb() Xor &HFFFFFF)
+
+        Return bmp
+    End Function
+
+
+    Private Sub Aktualisieren()
+        Try
+
+
+            SetComplementary()
+            hwi.ReloadAll()
+            Dim Result As String = ""
+            Dim Elements As Integer = 0
+            For Each item As HardwareMonitorEntry In hwi.Entries
+
+
+                If (item.Flags.ToString().Contains("LCD")) Then
+                    Result &= item.LocalizedSrcName & ":  " & item.Data.ToString("0") & " " & item.LocalizedSrcUnits & Environment.NewLine
+                    Elements += 1
+                End If
+
+            Next
+
+
+            Dim newsize As Integer = (Elements * 25) + 0
+            If (DesktopLocation.X > -32000) Then
+                Me.ClientSize = New System.Drawing.Size(Me.ClientSize.Width, newsize)
+            End If
+            TextBox2.Text = Result
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub TabPage1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub TabControl1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Aktualisieren()
+    End Sub
+
+    Private Sub Form1_MouseHover(sender As Object, e As EventArgs) Handles MyBase.MouseHover
+
+    End Sub
+
+    Private Sub Form1_MouseLeave(sender As Object, e As EventArgs) Handles MyBase.MouseLeave
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub Form1_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable
+        If (DesktopLocation.X > -32000) Then
+            Me.SetDesktopLocation(DesktopLocation.X - 7, DesktopLocation.Y - 30)
+            Me.ControlBox = True
+            active = True
+        End If
+    End Sub
+
+    Private Sub Form1_Deactivate(sender As Object, e As EventArgs) Handles MyBase.Deactivate
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        'Me.ControlBox = False
+
+        If (DesktopLocation.X > -32000) Then
+            Me.SetDesktopLocation(DesktopLocation.X + 7, DesktopLocation.Y + 30)
+        End If
+        active = False
+
+    End Sub
+
+    Private Sub Form1_Leave(sender As Object, e As EventArgs) Handles MyBase.Leave
+        'Me.ControlBox = False
+        'active = False
+    End Sub
+
+    Private Sub TextBox2_Click(sender As Object, e As EventArgs) Handles TextBox2.Click
+
+
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+
+        My.Settings.X = DesktopLocation.X
+        My.Settings.Y = DesktopLocation.Y
+        My.Settings.Save()
+    End Sub
+End Class
